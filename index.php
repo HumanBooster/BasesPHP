918cab7c7de37006e55541a68d45ad6930179644
HumanBooster <bclerc@humanbooster.com>
2015-01-06 11:46:54 +0100

    Ma première page web

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-Hello World !
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Mon premier Hello World</title>
+	</head>
+
+	<body>
+		<h1>Ma page web</h1>
+
+		<p>
+			Bonjour Benjamin !
+		</p>
+
+	</body>
+</html>
